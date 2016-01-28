458084b461c7a85ac8d18c258a1a9d215f0235f3
Rasmus Leifsgård <Leifsgard@outlook.com>
2016-01-28 04:26:12 +0100

    Någon övning på MSDN

---
+++ CalculatorHost/CalculatorHost/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace CalculatorHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAdress = new Uri("Http://localhost:8000/GettingStarted/");
+
+            ServiceHost selfHost = new ServiceHost(typeof(CalculatorService), baseAdress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+            }
+        }
+    }
+}
